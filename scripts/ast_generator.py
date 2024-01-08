eb330c3aa015b2f178b59f19faf380e6b55fda01
Swajeet Swarnkar <swajeetg768@gmail.com>
2024-01-08 21:47:27 +0530

    Improve and simplify ast_generator.py

--- scripts/ast_generator.py
+++ scripts/ast_generator.py
@@ -6,7 +6,7 @@ import os
 class Member:
     def __init__(self, mtype, mname):
         self.mname = mname
-        self.is_ptr = mtype[-1] == "*"
+        self.is_ptr = mtype.endswith("*")
         self.mtype = mtype[:-1] if self.is_ptr else mtype
 
 class Klass:
@@ -16,24 +16,17 @@ class Klass:
 
 class Include:
     def __init__(self, file):
-        self.is_std = file[-1] == "*"
+        self.is_std = file.endswith("*")
         self.file = file[:-1] if self.is_std else file
 
 def split_description(description):
-    splitted = description.split("=")
-    return (item.strip() for item in splitted)
+    return map(str.strip, description.split("="))
 
 def split_member_description(description):
-    splitted = description.split(" ")
-    return (item.strip() for item in splitted)
+    return map(str.strip, description.split(" "))
 
 def get_members(member_desc):
-    members = []
-    descriptions = member_desc.split(", ")
-    for description in descriptions:
-        mtype, mname = split_member_description(description)
-        members.append(Member(mtype, mname))
-    return members
+    return [Member(*split_member_description(description)) for description in member_desc.split(", ")]
 
 def get_includes(line):
     files_str = line.split("=")[1].strip()
@@ -45,14 +38,13 @@ def get_klass(line):
     return Klass(name, members)
 
 def read_file(desc_file):
-    lines = []
     with open(desc_file) as infile:
         lines = infile.readlines()
     includes = []
     klasses = []
     for line in lines:
         if line.startswith("%includes"):
-            includes.extend(get_includes(line)) 
+            includes.extend(get_includes(line))
         else:
             klasses.append(get_klass(line))
     return includes, klasses
@@ -61,7 +53,7 @@ class Writer:
     def __init__(self, file):
         self.file = file
 
-    def writeln(self, line = ""):
+    def writeln(self, line=""):
         self.file.write(line + "\n")
 
     def emmit_blank(self):
@@ -79,23 +71,20 @@ def generate_fwd_declarations_file(klasses, base, writer):
     writer.writeln("#define " + base.upper() + "_FWD_HPP")
     writer.emmit_blank()
     for klass in klasses:
-        writer.writeln("struct " + klass.name + ";")
+        writer.writeln(f"struct {klass.name};")
     writer.emmit_blank()
     writer.writeln("#endif")
 
 def get_parameter_type(member):
-    return ("std::shared_ptr<" + member.mtype + ">") if member.is_ptr else member.mtype
+    return f"std::shared_ptr<{member.mtype}>" if member.is_ptr else member.mtype
 
 def generate_class_decl(klass, base, writer):
-    parameters = ", ".join(
-        map(get_parameter_type, klass.members))
-    writer.writeln("struct " + klass.name + ": public " + base + " {")
+    parameters = ", ".join(map(get_parameter_type, klass.members))
+    writer.writeln(f"struct {klass.name}: public {base} {{")
     for member in klass.members:
-        writer.writeln(tabs(1) + get_parameter_type(member) + " " +
-            member.mname + ";")
-    writer.writeln(tabs(1) + klass.name +
-            "(" + parameters + ");")
-    writer.writeln(tabs(1) + "void accept(Mutable_state_visitor&);")
+        writer.writeln(f"{tabs(1)}{get_parameter_type(member)} {member.mname};")
+    writer.writeln(f"{tabs(1)}{klass.name}({parameters});")
+    writer.writeln(f"{tabs(1)}void accept(Mutable_state_visitor&);")
     writer.writeln("};")
     writer.emmit_blank()
 
@@ -109,14 +98,14 @@ def generate_header_file(includes, klasses, base, writer):
     writer.emmit_blank()
     for include in includes:
         if include.is_std:
-            writer.writeln("#include <" + include.file +">")
+            writer.writeln(f"#include <{include.file}>")
     writer.emmit_blank()
     for include in includes:
         if not include.is_std:
-            writer.writeln("#include \"" + include.file +"\"")
+            writer.writeln(f"#include \"{include.file}\"")
     writer.emmit_blank()
-    writer.writeln("struct " + base + " {")
-    writer.writeln(tabs(1) + "virtual void accept(Mutable_state_visitor&) = 0;")
+    writer.writeln(f"struct {base} {{")
+    writer.writeln(f"{tabs(1)}virtual void accept(Mutable_state_visitor&) = 0;")
     writer.writeln("};")
     writer.emmit_blank()
     for klass in klasses:
@@ -125,18 +114,16 @@ def generate_header_file(includes, klasses, base, writer):
     writer.writeln("#endif")
 
 def get_parameter(member):
-    return get_parameter_type(member) + " p_" + member.mname
+    return f"{get_parameter_type(member)} p_{member.mname}"
 
 def generate_class_impl(klass, base, writer):
-    parameters = ", ".join(
-        map(get_parameter, klass.members))
-    writer.writeln(klass.name + "::" + klass.name + "(" + parameters + "):")
-    initializer = ", ".join(
-        map(lambda member: member.mname + " {p_" + member.mname + "}", klass.members))
-    writer.writeln(tabs(1) + initializer + " {}")
+    parameters = ", ".join(map(get_parameter, klass.members))
+    writer.writeln(f"{klass.name}::{klass.name}({parameters}):")
+    initializer = ", ".join(map(lambda member: f"{member.mname} {{p_{member.mname}}}", klass.members))
+    writer.writeln(f"{tabs(1)}{initializer} {{}}")
     writer.emmit_blank()
-    writer.writeln("void " + klass.name + "::accept(Mutable_state_visitor& visitor) {")
-    writer.writeln(tabs(1) + "visitor.visit(*this);")
+    writer.writeln(f"void {klass.name}::accept(Mutable_state_visitor& visitor) {{")
+    writer.writeln(f"{tabs(1)}visitor.visit(*this);")
     writer.writeln("}")
     writer.emmit_blank()
 
@@ -147,13 +134,13 @@ def generate_impl_file(includes, klasses, base, writer):
     writer.emmit_blank()
     for include in includes:
         if include.is_std:
-            writer.writeln("#include <" + include.file +">")
+            writer.writeln(f"#include <{include.file}>")
     writer.emmit_blank()
     for include in includes:
         if not include.is_std:
-            writer.writeln("#include \"" + include.file +"\"")
+            writer.writeln(f"#include \"{include.file}\"")
     writer.emmit_blank()
-    writer.writeln("#include \"" + base.lower() + ".hpp\"")
+    writer.writeln(f"#include \"{base.lower()}.hpp\"")
     writer.emmit_blank()
     for klass in klasses:
         generate_class_impl(klass, base, writer)
@@ -162,27 +149,26 @@ def main():
     if len(sys.argv) != 4:
         print("Usage:", sys.argv[0], "<base>", "<desc_file>", "<output_dir>")
         exit(64)
-    
+
     _, base, desc_file, output_dir = sys.argv
 
     includes, klasses = read_file(desc_file)
-    
-    fwd_filename = os.path.join(output_dir, base.lower() + "_fwd.hpp")
-    file = open(fwd_filename, "w")
-    writer = Writer(file)
-    generate_fwd_declarations_file(klasses, base, writer)
-    file.close()
-    
-    header_filename = os.path.join(output_dir, base.lower() + ".hpp")
-    file = open(header_filename, "w")
-    writer = Writer(file)
-    generate_header_file(includes, klasses, base, writer)
-    file.close()
-    
-    impl_filename = os.path.join(output_dir, base.lower() + ".cpp")
-    file = open(impl_filename, "w")
-    writer = Writer(file)
-    generate_impl_file(includes, klasses, base, writer)
-    file.close()
 
-main()
+    fwd_filename = os.path.join(output_dir, base.lower() + "_fwd.hpp")
+    with open(fwd_filename, "w") as file:
+        writer = Writer(file)
+        generate_fwd_declarations_file(klasses, base, writer)
+
+    header_filename = os.path.join(output_dir, base.lower() + ".hpp")
+    with open(header_filename, "w") as file:
+        writer = Writer(file)
+        generate_header_file(includes, klasses, base, writer)
+
+    impl_filename = os.path.join(output_dir, base.lower() + ".cpp")
+    with open(impl_filename, "w") as file:
+        writer = Writer(file)
+        generate_impl_file(includes, klasses, base, writer)
+
+if __name__ == "__main__":
+    main()
+
